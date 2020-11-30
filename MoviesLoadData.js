986fde19f41f8f1c8d7d35f6cd4052f2426238ee
Abdullah <aalthawr@gmail.com>
2020-12-01 04:10:31 +0800

    Add code for document client and partiQL

---
+++ MoviesLoadData.js
@@ -0,0 +1,30 @@
+var AWS = require("aws-sdk");
+var fs = require('fs');
+
+AWS.config.update({
+    region: "us-east-1",
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+console.log("Importing movies into DynamoDB. Please wait.");
+
+var allMovies = JSON.parse(fs.readFileSync('moviedata.json', 'utf8'));
+allMovies.forEach(function(movie) {
+    var params = {
+        TableName: "Movies",
+        Item: {
+            "year":  movie.year,
+            "title": movie.title,
+            "info":  movie.info
+        }
+    };
+
+    docClient.put(params, function(err, data) {
+       if (err) {
+           console.error("Unable to add movie", movie.title, ". Error JSON:", JSON.stringify(err, null, 2));
+       } else {
+           console.log("PutItem succeeded:", movie.title);
+       }
+    });
+});
